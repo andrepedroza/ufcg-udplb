97b174cd8fad6bf855ebef0a1e082bb07fb8289e
André Pedroza <contact@andrepedroza.com>
2019-01-11 15:49:12 -0300

    Remove Unnecessary Code

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 
 CONF=/usr/local/lib/udp.tmpl
 
-if [ ! -s /usr/local/lib/udp.conf ]; then
-  cat <<EOF > $CONF
+cat <<EOF > $CONF
 stream {
   upstream backends {
 {{with service "$BACKEND"}}
@@ -20,11 +19,5 @@ stream {
   }
 }
 EOF
-fi
-
-if [ ! -e /etc/nginx/.configured ]; then
-  cat $CONF >> /etc/nginx/nginx.conf
-  touch /etc/nginx/.configured
-fi
 
 supervisord
